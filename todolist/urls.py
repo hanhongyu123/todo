9cb6a94ac33ebe7f1ecb9008397ab7ee6dd26efe
111 <1363362133@qq.com>
2019-06-18 15:30:44 +0800

    删除操作

---
+++ todolist/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import  views
+
+app_name='todolist'
+
+urlpatterns=[
+    path('home/',views.home,name='主页'),
+    path('edit/',views.edit,name='编辑'),
+    path('about/',views.about,name='关于'),
+    path('del/<forloop_counter>',views.delete,name='删除')
+]
+
+
--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
@@ -11,11 +11,11 @@ lst=[
 
 def home(request):
     global  lst
-    print(request.method)
+    # print(request.method)
     if request.method=='POST':
         content = request.POST.get('待办事项')
         print(content)
-        if not content and content.strip() == '':
+        if not content or content.strip() == '':
             return render(request, 'todolist/home.html', {'清单': lst,'警告':'请输入内容'})
         else:
             lst.append({'待办事项': content, '已完成': False})
@@ -25,8 +25,25 @@ def home(request):
         return render(request, 'todolist/home.html', {'清单': lst})
 
 
+
+
+
 def edit(request):
     return render(request, 'todolist/edit.html')
 
+
+
+
 def about(request):
     return render(request, 'todolist/about.html')
+
+
+
+# 删除操作
+def delete(request,forloop_counter):
+    global lst
+
+    forloop_counter=int(forloop_counter)-1
+    print(forloop_counter)
+    lst.pop(forloop_counter)
+    return redirect('todolist:主页')
