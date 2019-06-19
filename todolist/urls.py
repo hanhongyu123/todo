07b37a3e0589b9736dfed4c4e87c0a50daf0af54
111 <1363362133@qq.com>
2019-06-19 09:05:39 +0800

    代码格式化及删除使用post

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
-from . import  views
+from . import views
 
-app_name='todolist'
+app_name = 'todolist'
 
-urlpatterns=[
-    path('home/',views.home,name='主页'),
-    path('edit/<int:forloop_counter>',views.edit,name='编辑'),
-    path('about/',views.about,name='关于'),
-    path('del/<forloop_counter>',views.delete,name='删除')
-
-
+urlpatterns = [
+    path('home/', views.home, name='主页'),
+    path('edit/<int:forloop_counter>', views.edit, name='编辑'),
+    path('about/', views.about, name='关于'),
+    path('del/<forloop_counter>', views.delete, name='删除')
 
 ]
-
-
--- todolist/views.py
+++ todolist/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
-lst=[
-    {'待办事项':'吃饭','已完成':True},
-    {'待办事项':'逛街','已完成':False},
+lst = [
+    {'待办事项': '吃饭', '已完成': True},
+    {'待办事项': '逛街', '已完成': False},
 ]
 
 
-
 def home(request):
-    global  lst
+    global lst
     # print(request.method)
-    if request.method=='POST':
+    if request.method == 'POST':
         content = request.POST.get('待办事项')
         print(content)
         if not content or content.strip() == '':
-            return render(request, 'todolist/home.html', {'清单': lst,'警告':'请输入内容'})
+            return render(request, 'todolist/home.html', {'清单': lst, '警告': '请输入内容'})
         else:
             lst.append({'待办事项': content, '已完成': False})
             return render(request, 'todolist/home.html', {'清单': lst})
@@ -25,37 +24,31 @@ def home(request):
         return render(request, 'todolist/home.html', {'清单': lst})
 
 
-
-
-
-
-def edit(request,forloop_counter):
+def edit(request, forloop_counter):
     # print(type(forloop_counter))
-    if request.method=='POST':
+    if request.method == 'POST':
         content = request.POST.get('已修改事项')
         # print(content)
         if not content or content.strip() == '':
-            return render(request, 'todolist/edit.html', { '警告': '请输入内容'})
+            return render(request, 'todolist/edit.html', {'警告': '请输入内容'})
         else:
-           lst[forloop_counter-1]['待办事项']=content
-           return redirect('todolist:主页')
+            lst[forloop_counter - 1]['待办事项'] = content
+            return redirect('todolist:主页')
     else:
-        content =lst[forloop_counter-1]['待办事项']
-        return render(request, 'todolist/edit.html',{'待修改事项':content})
-
-
+        content = lst[forloop_counter - 1]['待办事项']
+        return render(request, 'todolist/edit.html', {'待修改事项': content})
 
 
 def about(request):
     return render(request, 'todolist/about.html')
 
 
-
 # 删除操作
-def delete(request,forloop_counter):
+def delete(request, forloop_counter):
     global lst
+    if request.method == 'POST':
+        forloop_counter = int(forloop_counter) - 1
+        # print(forloop_counter)
+        lst.pop(forloop_counter)
 
-    forloop_counter=int(forloop_counter)-1
-    # print(forloop_counter)
-    lst.pop(forloop_counter)
     return redirect('todolist:主页')
